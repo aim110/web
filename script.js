0cebd184bdd1317b5faa52ee641a28f349e6d2f2
aim110c <i.michailyukov@gmail.com>
2013-10-07 19:38:04 +0000

    - remove crossing after uncheking checkbox  - clean input field after adding text

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ function processForm() {
     var listItem = document.myCheckListForm.checklistItem.value;
     var listed = '<div id="item' + i + '"><input type="checkbox" onclick="crossedOut(\'item'+ i + '\')" />' + listItem + '</div>';
     document.getElementById("checkList").innerHTML += listed;
+    document.myCheckListForm.checklistItem.value = "";
 }
 
 function crossedOut(item) {
-    document.getElementById(item).className = "checked-off";
+    if (document.getElementById(item).className == "checked-off") {
+        document.getElementById(item).className = "plane";
+    } else {
+        document.getElementById(item).className = "checked-off";
+    }
 }
