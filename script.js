d2de41a0502c88222c15ebf020bc11e2d8e1370e
qqq <qqq@yandex-team.ru>
2013-10-09 22:12:21 +0400

    start using bootstrap

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ var i = 0;
 function processForm() {
     i++;
     var listItem = document.myCheckListForm.checklistItem.value;
-    var listed = '<div id="item' + i + '"><input type="checkbox" onclick="crossedOut(\'item'+ i + '\')" />' + listItem + '</div>';
-    document.getElementById("checkList").innerHTML += listed;
-    document.myCheckListForm.checklistItem.value = "";
+    if (listItem !== '') {
+        var listed = '<div id="item' + i + '"><input type="checkbox" class="plane" onclick="crossedOut(\'item'+ i + '\')" />' + listItem + '</div>';
+        document.getElementById("checkList").innerHTML += listed;
+        document.myCheckListForm.checklistItem.value = "";
+    }
 }
 
 function crossedOut(item) {
